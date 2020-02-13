c9304625be86f3e39dd420ca04131317a6100dc1
Nathan Martel <nathanmartel@gmail.com>
2020-02-12 18:36:01 -0800

    Weird testing added

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders text "UniWhal" in the body', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/UniWhal/i);
   expect(linkElement).toBeInTheDocument();
